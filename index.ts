239d5ab053b48b9526e5ddd9eb55129e7daef1af
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-01 08:19:23 +0100

    now exporting a function to be used as a library as well

--- index.ts
+++ index.ts
@@ -2,41 +2,46 @@ import { getFileContent } from "./library/file.ts";
 import { getStandardInput } from "./library/process.ts";
 import { transpose, getLines, getColumns } from "./library/string.ts";
 
-const fileContent = await getFileContent(Deno.args[0]) ?? await getStandardInput();
+export function alignMarkdownTable(separator: string, content: string): string {
+  const lines = getLines(content).map(line => {
+    return getColumns(separator, line);
+  });
 
-const columnSeparator = "|";
+  if (lines.length === 0 ) {
+    return "";
+  }
 
-const lines = getLines(fileContent).map(line => {
-  return getColumns(columnSeparator, line);
-});
+  const transposedLinesWithColumns = transpose(lines);
 
-if (lines.length === 0 ) {
-  Deno.exit(0);
+  const largestColumns = transposedLinesWithColumns.map(columns => {
+    return Math.max(...columns.map(column => {
+      return column.length;
+    }));
+  });
+
+  const separationLine = largestColumns.map(largestColumn => {
+    return "-".repeat(largestColumn - 1) + "-";
+  });
+
+  const paddedLines = lines.map(columns => {
+    return columns.map((column, columnIndex) => {
+      const padding = largestColumns[columnIndex];
+      return column.padEnd(padding, " ");
+    });
+  });
+
+  const joinedLines = paddedLines.map(columns => {
+    return columns.join(` ${separator} `);
+  });
+
+  const output = [joinedLines[0], separationLine.join(` ${separator} `), ...joinedLines.slice(1)].join("\n");
+
+  return output;
 }
 
-const transposedLinesWithColumns = transpose(lines);
+if (import.meta.main) {
+  const fileContent = await getFileContent(Deno.args[0]) ?? await getStandardInput();
+  const columnSeparator = "|";
 
-const largestColumns = transposedLinesWithColumns.map(columns => {
-  return Math.max(...columns.map(column => {
-    return column.length;
-  }));
-});
-
-const separationLine = largestColumns.map(largestColumn => {
-  return "-".repeat(largestColumn - 1) + "-";
-});
-
-const paddedLines = lines.map(columns => {
-  return columns.map((column, columnIndex) => {
-    const padding = largestColumns[columnIndex];
-    return column.padEnd(padding, " ");
-  });
-});
-
-const joinedLines = paddedLines.map(columns => {
-  return columns.join(` ${columnSeparator} `);
-});
-
-const output = [joinedLines[0], separationLine.join(` ${columnSeparator} `), ...joinedLines.slice(1)].join("\n");
-
-console.log(output);
+  console.log(alignMarkdownTable(columnSeparator, fileContent));
+}
