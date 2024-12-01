5f53c73f5f9becc403b4e746ea169e7a5d878d16
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-01 09:25:29 +0100

    moved the alignMarkdownTable function into the markdown library file

--- index.ts
+++ index.ts
@@ -1,55 +1,6 @@
 import { getFileContent } from "./library/file.ts";
 import { getStandardInput } from "./library/process.ts";
-import { transpose, getLines, getColumns } from "./library/string.ts";
-import { padArray, isSeparationLine } from "./library/array.ts";
-
-export function alignMarkdownTable(separator: string, content: string): string {
-  const lines = getLines(content).map(line => {
-    return getColumns(separator, line);
-  }).filter(line => {
-    return !isSeparationLine(line);
-  });
-
-  if (lines.length === 0 ) {
-    return "";
-  }
-
-  const largestLineLength = Math.max(...lines.map(line => {
-    return line.length;
-  }));
-
-  const paddedLines = lines.map(line => {
-    return padArray(line, largestLineLength, "");
-  });
-
-  const transposedLinesWithColumns = transpose(paddedLines);
-
-  const largestColumns = transposedLinesWithColumns.map(columns => {
-    return Math.max(...columns.map(column => {
-      return column.length;
-    }));
-  });
-
-
-  const separationLine = largestColumns.map(largestColumn => {
-    return "-".repeat(largestColumn - 1) + "-";
-  });
-
-  const paddedLinesWithColumns = lines.map(columns => {
-    return padArray(columns, largestLineLength, "").map((column, columnIndex) => {
-      const padding = largestColumns[columnIndex] ?? 0;
-      return column.padEnd(padding, " ");
-    })
-  });
-
-  const joinedLines = paddedLinesWithColumns.map(columns => {
-    return columns.join(` ${separator} `);
-  });
-
-  const output = [joinedLines[0], separationLine.join(` ${separator} `), ...joinedLines.slice(1)].join("\n");
-
-  return output;
-}
+import { alignMarkdownTable } from "./library/markdown.ts"
 
 if (import.meta.main) {
   const fileContent = await getFileContent(Deno.args[0]) ?? await getStandardInput();
